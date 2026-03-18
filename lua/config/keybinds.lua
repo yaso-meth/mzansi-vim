11631589cf147d3aa764558b3f6e6f21a4089e73
Yasien Mac Mini <yasienmeth@gmail.com>
2026-03-18 09:49:10 +0200

    Add homescreen dashboard

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,32 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>;", ":Dashboard<CR>", { desc = "Return to Dashboard", silent = true })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "netrw",
+  callback = function(event)
+    vim.schedule(function()
+      vim.keymap.set("n", "<Esc>", function()
+        vim.cmd("enew")
+        vim.cmd("Dashboard")
+      end, { 
+        buffer = event.buf, 
+        nowait = true,
+        desc = "Return to Dashboard" 
+      })
+    end)
+  end,
+})
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "netrw",
+  callback = function(event)
+    vim.schedule(function()
+      vim.keymap.set("n", "<leader>;", function()
+        vim.cmd("enew")
+        vim.cmd("Dashboard")
+      end, { 
+        buffer = event.buf, 
+        nowait = true,        desc = "Return to Dashboard" 
+      })
+    end)
+  end,
+})
