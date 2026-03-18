9c64b69498d233ad14f7109a5ef86af5afda5f69
Yasien Mac Mini <yasienmeth@gmail.com>
2026-03-18 12:44:44 +0200

    new help icon

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -33,7 +33,7 @@ return {
 					},
 					{ icon = "󱗘 ", desc = " Harpoon Search   ", key = "fl", action = "Telescope harpoon marks" },
 					{ icon = "󰒲 ", desc = " Lazy Manager     ", key = "z", action = "Lazy" },
-					{ icon = "ⓘ ", desc = " Help Tags        ", key = "fh", action = "Telescope help_tags" },
+					{ icon = "󰌵 ", desc = " Help Tags        ", key = "fh", action = "Telescope help_tags" },
 					{ icon = " ", desc = " Quit             ", key = "q", action = "qa" },
 				},
 				-- footer = { vim.fn.strftime("%Y-%m-%d %H:%M:%S") .. " • Mzansi Vim Kickstart" },
