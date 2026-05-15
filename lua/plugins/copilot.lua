2044bb94494c3cb7611da2104c47a76ab3a50397
Yasien Mac Mini <yasienmeth@gmail.com>
2026-05-15 12:10:57 +0200

    disable copilot on startup

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,6 +2,7 @@ return {
 	"github/copilot.vim",
 	lazy = false,
 	config = function()
+		vim.g.copilot_enabled = false
 		vim.g.copilot_no_tab_map = true
 		vim.keymap.set('i', '<C-l>', 'copilot#Accept("\\<CR>")', {
 			expr = true,
