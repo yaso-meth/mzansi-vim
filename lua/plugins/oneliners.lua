f0313398eb5e2715b44bb9157b82e5ae9de62774
Yasien Mac Mini <yasienmeth@gmail.com>
2026-06-30 09:13:42 +0200

    codex fix pt2

--- lua/plugins/oneliners.lua
+++ lua/plugins/oneliners.lua
@@ -18,6 +18,7 @@ return {
 			local autopairs = require("nvim-autopairs")
 			autopairs.setup({
 				check_ts = false,
+				map_cr = false,
 				ts_config = {
 					lua = { "string" },
 					dart = { "string_literal" }
